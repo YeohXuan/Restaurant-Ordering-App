c5df4d8dd4ca381786ab3823d8c8785219f33306
milocute <yorkxuan7@gmail.com>
2023-12-30 10:51:31 +0800

    Prettier

--- data.js
+++ data.js
@@ -1,229 +1,229 @@
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 export const breakfasts = [
-    {
-        food: 'Peanut Butter Bread',
-        specification: ['peanut butter', 'bread'],
-        price: 3.80,
-        image: "images/breakfasts/peanut-butter-bread.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'American Sandwich',
-        specification: ['cheese', 'onion', 'ham', 'salad'],
-        price: 7.90,
-        image: "images/breakfasts/sandwich.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Pancake',
-        specification: 'butter included',
-        price: 11.95,
-        image: "images/breakfasts/pancake.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Cereal',
-        specification: 'cereal before milk',
-        price: 6.50,
-        image: "images/breakfasts/cereal.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Half Boiled Egg',
-        specification: ['egg', 'white pepper'],
-        price: 2.20,
-        image: "images/breakfasts/half-boiled-egg.jpeg",
-        id: uuidv4()
-    }
-]
+  {
+    food: "Peanut Butter Bread",
+    specification: ["peanut butter", "bread"],
+    price: 3.8,
+    image: "images/breakfasts/peanut-butter-bread.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "American Sandwich",
+    specification: ["cheese", "onion", "ham", "salad"],
+    price: 7.9,
+    image: "images/breakfasts/sandwich.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Pancake",
+    specification: "butter included",
+    price: 11.95,
+    image: "images/breakfasts/pancake.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Cereal",
+    specification: "cereal before milk",
+    price: 6.5,
+    image: "images/breakfasts/cereal.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Half Boiled Egg",
+    specification: ["egg", "white pepper"],
+    price: 2.2,
+    image: "images/breakfasts/half-boiled-egg.jpeg",
+    id: uuidv4(),
+  },
+];
 
 export const mainDishes = [
-    {
-        food: 'Fried Rice',
-        specification: ['egg', 'carrot', 'pea', 'onion'],
-        price: 12.40,
-        image: "images/main-dishes/fried-rice.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Nasi Lemak',
-        specification: ['egg', 'sambal', 'peanut'],
-        price: 12.40,
-        image: "images/main-dishes/nasi-lemak.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Wonton Noodle',
-        specification: ['barbecue pork', 'veges'],
-        price: 9.50,
-        image: "images/main-dishes/wonton-noodle.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Tom Yum w Rice',
-        specification: ['prawn','mushroom','clam'],
-        price: 14.80,
-        image: "images/main-dishes/tomyum-rice.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Asam Laksa',
-        specification: ['cucumber','pineapple','onion'],
-        price: 14.80,
-        image: "images/main-dishes/asam-laksa.jpeg",
-        id: uuidv4()
-    }
-]
+  {
+    food: "Fried Rice",
+    specification: ["egg", "carrot", "pea", "onion"],
+    price: 12.4,
+    image: "images/main-dishes/fried-rice.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Nasi Lemak",
+    specification: ["egg", "sambal", "peanut"],
+    price: 12.4,
+    image: "images/main-dishes/nasi-lemak.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Wonton Noodle",
+    specification: ["barbecue pork", "veges"],
+    price: 9.5,
+    image: "images/main-dishes/wonton-noodle.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Tom Yum w Rice",
+    specification: ["prawn", "mushroom", "clam"],
+    price: 14.8,
+    image: "images/main-dishes/tomyum-rice.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Asam Laksa",
+    specification: ["cucumber", "pineapple", "onion"],
+    price: 14.8,
+    image: "images/main-dishes/asam-laksa.jpeg",
+    id: uuidv4(),
+  },
+];
 
 export const westerns = [
-    {
-        food: 'Spaghetti',
-        specification: ['tomato', 'meat sauce'],
-        price: 19.90,
-        image: "images/westerns/spaghetti.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Sirloin Steak',
-        specification: ['mashed potatoes', 'fries'],
-        price: 24,
-        image: "images/westerns/sirloin-steak.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Japanese Curry Rice',
-        specification: 'chicken chop',
-        price: 13.90,
-        image: "images/westerns/japanese-curry.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Fish n Chips',
-        specification: ['cod', 'fries', 'salad', 'lemon'],
-        price: 25,
-        image: "images/westerns/fish-n-chips.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Pasta',
-        specification: ['cheese', 'clam', 'tomato'],
-        price: 18,
-        image: "images/westerns/pasta.jpeg",
-        id: uuidv4()
-    }
-]
+  {
+    food: "Spaghetti",
+    specification: ["tomato", "meat sauce"],
+    price: 19.9,
+    image: "images/westerns/spaghetti.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Sirloin Steak",
+    specification: ["mashed potatoes", "fries"],
+    price: 24,
+    image: "images/westerns/sirloin-steak.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Japanese Curry Rice",
+    specification: "chicken chop",
+    price: 13.9,
+    image: "images/westerns/japanese-curry.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Fish n Chips",
+    specification: ["cod", "fries", "salad", "lemon"],
+    price: 25,
+    image: "images/westerns/fish-n-chips.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Pasta",
+    specification: ["cheese", "clam", "tomato"],
+    price: 18,
+    image: "images/westerns/pasta.jpeg",
+    id: uuidv4(),
+  },
+];
 
 export const addOns = [
-    {
-        food: 'White Rice',
-        specification: 'regular',
-        price: 2,
-        image: "images/add-ons/rice.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Extra Noodles',
-        specification: 'regular',
-        price: 2.50,
-        image: "images/add-ons/extra-noodle.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Fries',
-        specification: 'sauce included',
-        price: 4.50,
-        image: "images/add-ons/fries.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Mushroom Soup',
-        specification: ['mushroom', 'broth'],
-        price: 5,
-        image: "images/add-ons/mushroom-soup.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Curry Soup',
-        specification: 'potato',
-        price: 5,
-        image: "images/add-ons/curry-soup.jpeg",
-        id: uuidv4()
-    }
-]
+  {
+    food: "White Rice",
+    specification: "regular",
+    price: 2,
+    image: "images/add-ons/rice.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Extra Noodles",
+    specification: "regular",
+    price: 2.5,
+    image: "images/add-ons/extra-noodle.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Fries",
+    specification: "sauce included",
+    price: 4.5,
+    image: "images/add-ons/fries.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Mushroom Soup",
+    specification: ["mushroom", "broth"],
+    price: 5,
+    image: "images/add-ons/mushroom-soup.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Curry Soup",
+    specification: "potato",
+    price: 5,
+    image: "images/add-ons/curry-soup.jpeg",
+    id: uuidv4(),
+  },
+];
 
 export const beverages = [
-    {
-        food: 'Kopi O',
-        specification: 'hot',
-        price: 8,
-        image: "images/beverages/kopi-o.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Americano',
-        specification: 'hot',
-        price: 6.50,
-        image: "images/beverages/americano.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Lemon Tea',
-        specification: 'cold',
-        price: 8.20,
-        image: "images/beverages/lemon-tea.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Three Layer Milktea',
-        specification: 'cold',
-        price: 12,
-        image: "images/beverages/three-layer-milktea.png",
-        id: uuidv4()
-    },
-    {
-        food: 'Choco Ice Blended',
-        specification: 'cold',
-        price: 12.90,
-        image: "images/beverages/chocolate-ice-blended.jpeg",
-        id: uuidv4()
-    }
-]
+  {
+    food: "Kopi O",
+    specification: "hot",
+    price: 8,
+    image: "images/beverages/kopi-o.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Americano",
+    specification: "hot",
+    price: 6.5,
+    image: "images/beverages/americano.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Lemon Tea",
+    specification: "cold",
+    price: 8.2,
+    image: "images/beverages/lemon-tea.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Three Layer Milktea",
+    specification: "cold",
+    price: 12,
+    image: "images/beverages/three-layer-milktea.png",
+    id: uuidv4(),
+  },
+  {
+    food: "Choco Ice Blended",
+    specification: "cold",
+    price: 12.9,
+    image: "images/beverages/chocolate-ice-blended.jpeg",
+    id: uuidv4(),
+  },
+];
 
 export const desserts = [
-    {
-        food: 'Chocolate Cake',
-        specification: '-',
-        price: 5,
-        image: "images/desserts/chocolate-cake.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Burnt Cheesecake',
-        specification: 'cheese',
-        price: 5,
-        image: "images/desserts/burnt-cheesecake.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Pudding',
-        specification: 'white cream',
-        price: 4.50,
-        image: "images/desserts/pudding.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Brownies',
-        specification: 'chocolate',
-        price: 4.80,
-        image: "images/desserts/brownies.jpeg",
-        id: uuidv4()
-    },
-    {
-        food: 'Mochi',
-        specification: 'bean paste',
-        price: 6.90,
-        image: "images/desserts/mochi.jpeg",
-        id: uuidv4()
-    }
-]
+  {
+    food: "Chocolate Cake",
+    specification: "-",
+    price: 5,
+    image: "images/desserts/chocolate-cake.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Burnt Cheesecake",
+    specification: "cheese",
+    price: 5,
+    image: "images/desserts/burnt-cheesecake.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Pudding",
+    specification: "white cream",
+    price: 4.5,
+    image: "images/desserts/pudding.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Brownies",
+    specification: "chocolate",
+    price: 4.8,
+    image: "images/desserts/brownies.jpeg",
+    id: uuidv4(),
+  },
+  {
+    food: "Mochi",
+    specification: "bean paste",
+    price: 6.9,
+    image: "images/desserts/mochi.jpeg",
+    id: uuidv4(),
+  },
+];
--- index.js
+++ index.js
@@ -1,134 +1,152 @@
-import { breakfasts, mainDishes, westerns, addOns, beverages, desserts } from "./data.js"
+import {
+  breakfasts,
+  mainDishes,
+  westerns,
+  addOns,
+  beverages,
+  desserts,
+} from "./data.js";
 
-const foods = breakfasts.concat(mainDishes, westerns, addOns, beverages, desserts)
-let orderedItemArr = []
+const foods = breakfasts.concat(
+  mainDishes,
+  westerns,
+  addOns,
+  beverages,
+  desserts
+);
+let orderedItemArr = [];
 
-document.addEventListener('click', e => {
-    if (e.target.dataset.add) {
-        addToCart(e.target.dataset.add)
-    } else if (e.target.id === 'down-btn' || e.target.id === 'down-icon') {
-        closeContainer()
-    } else if (e.target.id === 'up-btn' || e.target.id === 'up-icon') {
-        openContainer()
-    } else if (e.target.dataset.remove) {
-        removeItem(e.target.dataset.remove)
-    } else if (e.target.id === 'complete-btn') {
-        completeOrder()
-    } else if (e.target.id === 'close-modal-btn') {
-        closeModal()
-    } else if (e.target.id === 'pay-btn') {
-        completePayment()
-    } else if (e.target.id === 'continue-btn') {
-        continueOrdering()
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.add) {
+    addToCart(e.target.dataset.add);
+  } else if (e.target.id === "down-btn" || e.target.id === "down-icon") {
+    closeContainer();
+  } else if (e.target.id === "up-btn" || e.target.id === "up-icon") {
+    openContainer();
+  } else if (e.target.dataset.remove) {
+    removeItem(e.target.dataset.remove);
+  } else if (e.target.id === "complete-btn") {
+    completeOrder();
+  } else if (e.target.id === "close-modal-btn") {
+    closeModal();
+  } else if (e.target.id === "pay-btn") {
+    completePayment();
+  } else if (e.target.id === "continue-btn") {
+    continueOrdering();
+  }
+});
+
+function addToCart(foodId) {
+  foods.forEach((food) => {
+    if (food.id === foodId) {
+      orderedItemArr.push(food);
     }
-})
-
-function addToCart(foodId){
-    foods.forEach(food => {
-        if (food.id === foodId) {
-            orderedItemArr.push(food)
-        }
-    })
-    renderOrderedItem()
-    totalPrice(foods)
+  });
+  renderOrderedItem();
+  totalPrice(foods);
 }
 
-function renderOrderedItem(){
-    document.getElementById('order-container').innerHTML = ''
-    
-    orderedItemArr.forEach(item => {
-        document.getElementById('container').style.display = 'block'
-        document.getElementById('menu').style.paddingBottom = '250px'
-        document.getElementById('order-container').innerHTML += `
+function renderOrderedItem() {
+  document.getElementById("order-container").innerHTML = "";
+
+  orderedItemArr.forEach((item) => {
+    document.getElementById("container").style.display = "block";
+    document.getElementById("menu").style.paddingBottom = "250px";
+    document.getElementById("order-container").innerHTML += `
             <div class="orders">
                 <div class="item-container">
                     <div class="item">
                         <h3>${item.food}</h3>
-                        <button class="remove-btn" data-remove="${item.id}">remove</button>
+                        <button class="remove-btn" data-remove="${
+                          item.id
+                        }">remove</button>
                     </div>
                     <h3>$${item.price.toFixed(2)}</h3>
                 </div>
             </div>
-        `
-    })
+        `;
+  });
 }
 
-function totalPrice(){
-    let totalPrice = 0
-    orderedItemArr.forEach(item => {
-        totalPrice += item.price
-    })
-    const serviceTax = totalPrice * 6/100
-    const finalPrice = totalPrice + serviceTax 
+function totalPrice() {
+  let totalPrice = 0;
+  orderedItemArr.forEach((item) => {
+    totalPrice += item.price;
+  });
+  const serviceTax = (totalPrice * 6) / 100;
+  const finalPrice = totalPrice + serviceTax;
 
-    document.getElementById('service-tax').textContent = '+$' + serviceTax.toFixed(2)
-    document.getElementById('total-price').textContent = '$' + finalPrice.toFixed(2)
+  document.getElementById("service-tax").textContent =
+    "+$" + serviceTax.toFixed(2);
+  document.getElementById("total-price").textContent =
+    "$" + finalPrice.toFixed(2);
 }
 
-function closeContainer(){
-    document.getElementById('item-container').style.display = 'none'
-    document.getElementById('service-container').style.display = 'none'
-    document.getElementById('up-icon').style.display = 'block'
-    document.getElementById('menu').style.paddingBottom = '170px'
+function closeContainer() {
+  document.getElementById("item-container").style.display = "none";
+  document.getElementById("service-container").style.display = "none";
+  document.getElementById("up-icon").style.display = "block";
+  document.getElementById("menu").style.paddingBottom = "170px";
 }
 
-function openContainer(){
-    document.getElementById('item-container').style.display = 'block'
-    document.getElementById('service-container').style.display = 'flex'
-    document.getElementById('up-icon').style.display = 'none'
-    document.getElementById('menu').style.paddingBottom = '250px'
+function openContainer() {
+  document.getElementById("item-container").style.display = "block";
+  document.getElementById("service-container").style.display = "flex";
+  document.getElementById("up-icon").style.display = "none";
+  document.getElementById("menu").style.paddingBottom = "250px";
 }
 
-function removeItem(itemId){
-    let removedIndex = -1
+function removeItem(itemId) {
+  let removedIndex = -1;
 
-    orderedItemArr.forEach((item, index) => {
-        if (item.id === itemId) {
-            removedIndex = index
-        }
-    })
-
-    if (removedIndex !== -1) {
-        orderedItemArr.splice(removedIndex, 1)
+  orderedItemArr.forEach((item, index) => {
+    if (item.id === itemId) {
+      removedIndex = index;
     }
+  });
 
-    if (orderedItemArr.length === 0) {
-        document.getElementById('container').style.display = 'none'
-        document.getElementById('menu').style.paddingBottom = '0'
-    }
+  if (removedIndex !== -1) {
+    orderedItemArr.splice(removedIndex, 1);
+  }
 
-    totalPrice()
-    renderOrderedItem()
+  if (orderedItemArr.length === 0) {
+    document.getElementById("container").style.display = "none";
+    document.getElementById("menu").style.paddingBottom = "0";
+  }
+
+  totalPrice();
+  renderOrderedItem();
 }
 
-function completeOrder(){
-    document.getElementById('card-details-container').style.display = 'block'
-
+function completeOrder() {
+  document.getElementById("card-details-container").style.display = "block";
 }
 
-function closeModal(){
-    document.getElementById('card-details-container').style.display = 'none'
+function closeModal() {
+  document.getElementById("card-details-container").style.display = "none";
 }
 
-function completePayment(){
-    orderedItemArr = []
-    document.getElementById('container').style.display = 'none'
-    document.getElementById('card-details-container').style.display = 'none'
-    document.getElementById('continue-container').style.display = 'block'
-    document.getElementById('message').innerHTML = `
-       Thanks, <span class="name">${document.getElementById('name-input').value}</span>! Your order is on its way!
-    `
+function completePayment() {
+  orderedItemArr = [];
+  document.getElementById("container").style.display = "none";
+  document.getElementById("card-details-container").style.display = "none";
+  document.getElementById("continue-container").style.display = "block";
+  document.getElementById("message").innerHTML = `
+       Thanks, <span class="name">${
+         document.getElementById("name-input").value
+       }</span>! Your order is on its way!
+    `;
 }
 
-function continueOrdering(){
-    document.getElementById('continue-container').style.display = 'none'
+function continueOrdering() {
+  document.getElementById("continue-container").style.display = "none";
 }
 
-function renderText(types, typeEl){
-    types.forEach(type => {
-        const {image, food, specification, price, id} = type
-        
-        typeEl.innerHTML += `
+function renderText(types, typeEl) {
+  types.forEach((type) => {
+    const { image, food, specification, price, id } = type;
+
+    typeEl.innerHTML += `
         <div class="option">
         <div class="food">
         <img src=${image} class="imgs" draggable="false">
@@ -140,48 +158,48 @@ function renderText(types, typeEl){
         </div>
         <button class="add-to-cart" data-add="${id}">+</button>
         </div>    
-        `
-    })
+        `;
+  });
 }
 
-function render(){
-    renderBreakfast()
-    renderMainDish()
-    renderWestern()
-    renderAddOns()
-    renderBeverages()
-    renderDesserts()
+function render() {
+  renderBreakfast();
+  renderMainDish();
+  renderWestern();
+  renderAddOns();
+  renderBeverages();
+  renderDesserts();
 }
 
-render()
+render();
 
-function renderBreakfast(){
-    const breakfastsEl = document.getElementById('breakfasts')
-    breakfastsEl.innerHTML += `<h1 class="category-name">Breakfasts</h1>`
-    renderText(breakfasts, breakfastsEl)
+function renderBreakfast() {
+  const breakfastsEl = document.getElementById("breakfasts");
+  breakfastsEl.innerHTML += `<h1 class="category-name">Breakfasts</h1>`;
+  renderText(breakfasts, breakfastsEl);
 }
-function renderMainDish(){
-    const mainDishesEl = document.getElementById('main-dishes')
-    mainDishesEl.innerHTML += `<h1 class="category-name">Main Dishes</h1>`
-    renderText(mainDishes, mainDishesEl)
+function renderMainDish() {
+  const mainDishesEl = document.getElementById("main-dishes");
+  mainDishesEl.innerHTML += `<h1 class="category-name">Main Dishes</h1>`;
+  renderText(mainDishes, mainDishesEl);
 }
-function renderWestern(){
-    const westernsEl = document.getElementById('westerns')
-    westernsEl.innerHTML += `<h1 class="category-name">Westerns</h1>`
-    renderText(westerns, westernsEl)
+function renderWestern() {
+  const westernsEl = document.getElementById("westerns");
+  westernsEl.innerHTML += `<h1 class="category-name">Westerns</h1>`;
+  renderText(westerns, westernsEl);
 }
-function renderAddOns(){
-    const addOnsEl = document.getElementById('add-ons')
-    addOnsEl.innerHTML += `<h1 class="category-name">Add-ons</h1>`
-    renderText(addOns, addOnsEl)
+function renderAddOns() {
+  const addOnsEl = document.getElementById("add-ons");
+  addOnsEl.innerHTML += `<h1 class="category-name">Add-ons</h1>`;
+  renderText(addOns, addOnsEl);
 }
-function renderBeverages(){
-    const beveragesEl = document.getElementById('beverages')
-    beveragesEl.innerHTML += `<h1 class="category-name">Beverages</h1>`
-    renderText(beverages, beveragesEl)
+function renderBeverages() {
+  const beveragesEl = document.getElementById("beverages");
+  beveragesEl.innerHTML += `<h1 class="category-name">Beverages</h1>`;
+  renderText(beverages, beveragesEl);
 }
-function renderDesserts(){
-    const dessertsEl = document.getElementById('desserts')
-    dessertsEl.innerHTML += `<h1 class="category-name">Desserts</h1>`
-    renderText(desserts, dessertsEl)
+function renderDesserts() {
+  const dessertsEl = document.getElementById("desserts");
+  dessertsEl.innerHTML += `<h1 class="category-name">Desserts</h1>`;
+  renderText(desserts, dessertsEl);
 }
